03bc0241f241f3e358c3e8992aca1095c64a9e31
arch-dev <sebastiano.galeazzo@gmail.com>
2024-07-22 20:30:07 +0200

    chore: use JvmTarget class to increase type safety

--- build-logic/convention/src/main/kotlin/com/archsoftware/afoil/KotlinAndroid.kt
+++ build-logic/convention/src/main/kotlin/com/archsoftware/afoil/KotlinAndroid.kt
@@ -5,6 +5,7 @@ import org.gradle.api.JavaVersion
 import org.gradle.api.Project
 import org.gradle.kotlin.dsl.provideDelegate
 import org.gradle.kotlin.dsl.withType
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 /**
@@ -36,8 +37,8 @@ private fun Project.configureKotlin() {
     // Use withType to workaround https://youtrack.jetbrains.com/issue/KT-55947
     tasks.withType<KotlinCompile>().configureEach {
         kotlinOptions {
-            // Set JVM target to 11
-            jvmTarget = JavaVersion.VERSION_17.toString()
+            // Set JVM target to 17
+            jvmTarget = JvmTarget.JVM_17.target
             // Treat all Kotlin warnings as errors (disabled by default)
             // Override by setting warningsAsErrors=true in your ~/.gradle/gradle.properties
             val warningsAsErrors: String? by project
