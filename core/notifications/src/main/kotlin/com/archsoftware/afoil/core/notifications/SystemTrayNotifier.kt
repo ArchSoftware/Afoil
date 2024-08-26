f7134c2ee7312fb4900ff94dd5185c5077ad11b5
arch-dev <sebastiano.galeazzo@gmail.com>
2024-08-26 09:35:19 +0200

    feat: add deep link to computation monitor screen

--- core/notifications/src/main/kotlin/com/archsoftware/afoil/core/notifications/SystemTrayNotifier.kt
+++ core/notifications/src/main/kotlin/com/archsoftware/afoil/core/notifications/SystemTrayNotifier.kt
@@ -3,15 +3,24 @@ package com.archsoftware.afoil.core.notifications
 import android.app.Notification
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.ComponentName
 import android.content.Context
+import android.content.Intent
 import android.os.Build
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
+import androidx.core.app.TaskStackBuilder
+import androidx.core.net.toUri
 import dagger.hilt.android.qualifiers.ApplicationContext
 import javax.inject.Inject
 import javax.inject.Singleton
 
 private const val COMPUTATION_SERVICE_NOTIFICATION_CHANNEL_ID = "computation_service_channel"
+private const val COMPUTATION_SERVICE_NOTIFICATION_REQUEST_CODE = 0
+private const val TARGET_ACTIVITY_NAME = "com.archsoftware.afoil.MainActivity"
+private const val DEEP_LINK_SCHEME_AND_HOST = "https://www.afoil.archsoftware.com"
+private const val COMPUTATION_MONITOR = "computationmonitor"
 private const val PROGRESS_MAX = 100
 
 @Singleton
@@ -42,7 +51,12 @@ class SystemTrayNotifier @Inject constructor(
         computationServiceNotificationBuilder =
             NotificationCompat.Builder(context, COMPUTATION_SERVICE_NOTIFICATION_CHANNEL_ID).apply {
                 setSmallIcon(R.drawable.core_notifications_ic_afoil_notifications)
-                if (computationName != null) setContentTitle(computationName)
+                if (computationName != null) {
+                    setContentTitle(computationName)
+                    setContentIntent(computationMonitorPendingIntent(computationName))
+                }
+                // Show notification immediately
+                foregroundServiceBehavior = NotificationCompat.FOREGROUND_SERVICE_IMMEDIATE
             }
         return computationServiceNotificationBuilder.build()
     }
@@ -65,6 +79,26 @@ class SystemTrayNotifier @Inject constructor(
             val notificationManager: NotificationManager =
                 context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
             notificationManager.createNotificationChannel(channel)
+        }
+    }
+
+    private fun computationMonitorPendingIntent(
+        computationName: String
+    ): PendingIntent? {
+        val deepLinkIntent = Intent().apply {
+            action = Intent.ACTION_VIEW
+            data = "$DEEP_LINK_SCHEME_AND_HOST/$COMPUTATION_MONITOR/$computationName".toUri()
+            component = ComponentName(
+                /* pkg = */ context.packageName,
+                /* cls = */ TARGET_ACTIVITY_NAME,
+            )
+        }
+        return TaskStackBuilder.create(context).run {
+            addNextIntentWithParentStack(deepLinkIntent)
+            getPendingIntent(
+                /* requestCode = */ COMPUTATION_SERVICE_NOTIFICATION_REQUEST_CODE,
+                /* flags = */ PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
+            )
         }
     }
 
--- feature/computationmonitor/src/main/kotlin/com/archsoftware/afoil/feature/computationmonitor/navigation/ComputationMonitorNavigation.kt
+++ feature/computationmonitor/src/main/kotlin/com/archsoftware/afoil/feature/computationmonitor/navigation/ComputationMonitorNavigation.kt
@@ -6,10 +6,12 @@ import androidx.navigation.NavOptions
 import androidx.navigation.NavType
 import androidx.navigation.compose.composable
 import androidx.navigation.navArgument
+import androidx.navigation.navDeepLink
 import com.archsoftware.afoil.feature.computationmonitor.ComputationMonitorScreen
 
 const val PROJECT_NAME_ARG = "projectName"
 const val COMPUTATION_MONITOR_ROUTE = "computationmonitor_route"
+private const val DEEP_LINK_URI_PATTERN = "https://www.afoil.archsoftware.com/computationmonitor/{$PROJECT_NAME_ARG}"
 
 fun NavController.navigateToComputationMonitor(
     projectName: String,
@@ -26,7 +28,8 @@ fun NavGraphBuilder.computationMonitorScreen(
 ) {
     composable(
         route = "$COMPUTATION_MONITOR_ROUTE/{$PROJECT_NAME_ARG}",
-        arguments = listOf(navArgument(PROJECT_NAME_ARG) { type = NavType.StringType })
+        arguments = listOf(navArgument(PROJECT_NAME_ARG) { type = NavType.StringType }),
+        deepLinks = listOf(navDeepLink { uriPattern = DEEP_LINK_URI_PATTERN })
     ) {
         ComputationMonitorScreen(
             onNavigateUp = onNavigateUp,
