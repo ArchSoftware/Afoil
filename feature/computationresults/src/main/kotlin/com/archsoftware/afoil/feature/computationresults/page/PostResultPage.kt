f1dc95db826744081d611025c8a710fa760fbc1f
arch-dev <sebastiano.galeazzo@gmail.com>
2024-09-08 17:05:57 +0200

    fix: use SubcomposeAsyncImage to switch between loading, error and image contents
    Previously this was done through AnimatedContent but that was preventing the request
    from starting correctly, thus the image was never being loaded.

--- feature/computationresults/src/main/kotlin/com/archsoftware/afoil/feature/computationresults/page/PostResultPage.kt
+++ feature/computationresults/src/main/kotlin/com/archsoftware/afoil/feature/computationresults/page/PostResultPage.kt
@@ -1,6 +1,5 @@
 package com.archsoftware.afoil.feature.computationresults.page
 
-import androidx.compose.animation.AnimatedContent
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -8,10 +7,6 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
@@ -19,8 +14,7 @@ import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.core.net.toUri
-import coil.compose.AsyncImage
-import coil.compose.AsyncImagePainter
+import coil.compose.SubcomposeAsyncImage
 import com.archsoftware.afoil.core.designsystem.theme.AfoilTheme
 import com.archsoftware.afoil.core.model.AfoilProjectPostResult
 import com.archsoftware.afoil.core.ui.PageWrapper
@@ -31,51 +25,39 @@ fun PostResultPage(
     postResult: AfoilProjectPostResult,
     modifier: Modifier = Modifier
 ) {
-    var asyncImageState: AsyncImagePainter.State by remember {
-        mutableStateOf(AsyncImagePainter.State.Empty)
-    }
     PageWrapper(
         title = stringResource(id = postResult.nameId),
         modifier = modifier
     ) {
-        AnimatedContent(
-            targetState = asyncImageState,
-            label = "postResultPageAnimation"
-        ) { targetState ->
-            when (targetState) {
-                is AsyncImagePainter.State.Error -> {
-                    Column(
-                        horizontalAlignment = Alignment.CenterHorizontally,
-                        verticalArrangement = Arrangement.Center,
-                        modifier = Modifier.fillMaxSize()
+        SubcomposeAsyncImage(
+            model = postResult.uri.toUri(),
+            loading = {
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Center,
+                    modifier = Modifier.fillMaxSize()
 
-                    ) {
-                        Text(text = "Error loading plot")
-                    }
-                }
-                is AsyncImagePainter.State.Success -> {
-                    AsyncImage(
-                        model = postResult.uri.toUri(),
-                        contentDescription = stringResource(id = postResult.nameId),
-                        onState = { state -> asyncImageState = state }
+                ) {
+                    CircularProgressIndicator()
+                    Text(
+                        text = "Loading plot...",
+                        modifier = Modifier.padding(top = 8.dp)
                     )
                 }
-                else -> {
-                    Column(
-                        horizontalAlignment = Alignment.CenterHorizontally,
-                        verticalArrangement = Arrangement.Center,
-                        modifier = Modifier.fillMaxSize()
+            },
+            error = {
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Center,
+                    modifier = Modifier.fillMaxSize()
 
-                    ) {
-                        CircularProgressIndicator()
-                        Text(
-                            text = "Loading plot...",
-                            modifier = Modifier.padding(top = 8.dp)
-                        )
-                    }
+                ) {
+                    Text(text = "Error loading plot")
                 }
-            }
-        }
+            },
+            contentDescription = stringResource(id = postResult.nameId),
+            modifier = Modifier.padding(top = 16.dp)
+        )
     }
 }
 
